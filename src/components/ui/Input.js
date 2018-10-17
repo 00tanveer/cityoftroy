71961d05789428ca9dc4b9631d35d38ee9ac9916
00tanveer <tanveer_fh09@yahoo.com>
2018-10-18 01:03:46 +0600

    added blog title input

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledInput = styled.input`
+    display: block;
+    margin: 0 auto;
+    width: 400px;
+    height: 40px;
+    font-size: 2rem;
+    margin-top: 40px;
+    padding: 10px;
+`
 const Input = props => {
-    const Input = styled.input`
-      display: block;
-			margin: 0 auto;
-    `
+    
     return (
-			<Input 
-				placeholder/>
+			<StyledInput 
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}/>
     );
 }
 
