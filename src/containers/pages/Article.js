28d57336fc59ebdaf037e0fb9ed2099b75a8b967
00tanveer <tanveer_fh09@yahoo.com>
2018-10-27 01:34:42 +0600

    loaded the article body into quill container :zap:

--- src/containers/pages/Article.js
+++ src/containers/pages/Article.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import ReactQuill from 'react-quill';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import Header from '../../components/ui/header/Header';
@@ -26,16 +27,35 @@ class Article extends React.Component {
 		this.state = {
 			title: match.params.title,
 			blogId: blogId,
-			blog: {}
+			blog: {},
+			//quill states
+			editorHtml: ''
 		}
+		this.quillRef = null;
+		this.reactQuillRef = null;
+		this.attachQuillRefs = this.attachQuillRefs.bind(this);
 	}
 
 	componentDidMount() {
 		axios.get(`/blogs/${this.state.blogId}`).then(res => {
 			console.log(res.data.data.docs[0]);
 			this.setState({blog: res.data.data.docs[0]});
+			this.quillRef.setContents(res.data.data.docs[0].delta_ops);
+			this.quillRef.disable();
 		});
 	}
+
+	componentDidUpdate() {
+		this.attachQuillRefs();
+	}
+
+	attachQuillRefs = () => {
+		if (typeof this.reactQuillRef.getEditor !== 'function') return;
+		if (this.quillRef != null) return;
+
+		const quillRef = this.reactQuillRef.getEditor();
+		if (quillRef != null) this.quillRef = quillRef;
+	};
 
 	render() {
 		let date = new Date(this.state.blog.date);
@@ -52,11 +72,22 @@ class Article extends React.Component {
 		let html = converter.convert();
 		const ArticleBody = ReactHtmlParser(html);
 		console.log(ArticleBody);
+		//quill config
+		const modules = {
+			toolbar : false
+		}
 		return(
 			<StyledContainer>
 				<Header />
 				<Title>{this.state.blog.title}</Title>
-				{ArticleBody}
+				{/* {ArticleBody} */}
+				<ReactQuill 
+					ref={el => {
+						this.reactQuillRef = el;
+					}}
+					modules={modules}
+					defaultValue={this.state.editorHtml}
+				/>
 				<Footer />
 			</StyledContainer>
 		);
