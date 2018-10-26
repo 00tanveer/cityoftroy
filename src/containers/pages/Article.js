a06065a04d413dd0071196320d38751b24b7dcfc
00tanveer <tanveer_fh09@yahoo.com>
2018-10-27 01:04:02 +0600

    fetch blog from server into Article component

--- src/containers/pages/Article.js
+++ src/containers/pages/Article.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 class Article extends React.Component {
 	constructor(props) {
 		super(props);
 		const {match} = this.props;
-		console.log(match);
+		let blogId = match.params.title.split('-');
+		blogId = blogId[blogId.length - 1];
+		this.state = {
+			title: match.params.title,
+			blogId: blogId,
+			blog: {}
+		}
+	}
+
+	componentDidMount() {
+		axios.get(`/blogs/${this.state.blogId}`).then(res => {
+			console.log(res.data.data.docs[0]);
+			this.setState({blog: res.data.data.docs[0]});
+		});
 	}
 
 	render() {
