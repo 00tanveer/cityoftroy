1fc6eacb7950697aee225888a607a6e67f4ad7d2
00tanveer <tanveer_fh09@yahoo.com>
2018-10-28 01:54:12 +0600

    added main panel

--- src/containers/pages/Admin.js
+++ src/containers/pages/Admin.js
@@ -2,6 +2,8 @@ import React from 'react';
 import withAuth from '../../components/HOC/withAuth';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Card from '../../components/ui/blog_card/Card';
 
 const StyledContainer = styled.div`
 
@@ -13,6 +15,7 @@ const Title = styled.p`
   font-family: "Great Vibes", cursive;
   font-weight: 300;
 	margin-top: 20px;
+	margin-bottom: 20px;
   text-shadow: 0 0 3px white;
   text-align: center;
   letter-spacing: 1rem;
@@ -23,7 +26,7 @@ const SideBar = styled.div`
 	position: fixed;
 	left: 0;
 	height: 100%;
-	width: 200px;
+	width: 25vw;
 	background-color: rgb(8,8,8);
 	.row {
 		color: grey;
@@ -53,21 +56,71 @@ const SideBar = styled.div`
 	}
 `;
 
+const MainPanel = styled.div`
+	width: 75vw;
+	float: right;
+	display: flex;
+	flex-direction: column;
+	justify-content: start;
+	align-items: center;
+	select {
+		height: 30px;
+		width: 100px;
+	}
+`;
+
 class Admin extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			selected: 'drafts'
+			selected: 'drafts',
+			genre: 'fashion',
+			blogs: [],
 		}
 		this.rowClickHandler = this.rowClickHandler.bind(this);
+		this.selectOnChangeHandler = this.selectOnChangeHandler.bind(this);
+	}
+
+	componentDidMount() {
+		axios.get(`/blogs/all/${this.state.genre}?posted=${this.state.selected==='posted'}`).then(res => {
+			if (res.data.data.length !==0) {
+				this.setState({
+					blogs: res.data.data
+				})
+			}
+		})
 	}
 
 	rowClickHandler() {
 		if (this.state.selected === 'drafts') {
-			this.setState({ selected: 'posted'});
+			this.setState({ selected: 'posted'}, () => {
+				axios.get(`/blogs/all/${this.state.genre}?posted=${this.state.selected==='posted'}`).then(res => {
+					console.log('yo');
+					this.setState({
+						blogs: res.data.data
+					})
+				})
+			});
 		} else {
-			this.setState({ selected: 'drafts' });
+			this.setState({ selected: 'drafts' }, () => {
+				axios.get(`/blogs/all/${this.state.genre}?posted=${this.state.selected==='posted'}`).then(res => {
+					console.log('yoyo');
+					this.setState({
+						blogs: res.data.data
+					})
+				})
+			});
 		}
+	}
+
+	selectOnChangeHandler(e) {
+		this.setState({genre: e.target.value}, () => {
+			axios.get(`/blogs/all/${this.state.genre}?posted=${this.state.selected==='posted'}`).then(res => {
+				this.setState({
+					blogs: res.data.data
+				})
+			})
+		})
 	}
 
 	render() {
@@ -81,6 +134,24 @@ class Admin extends React.Component {
 					<div className="divider"></div>
 					<div onClick={this.rowClickHandler} className="row" id="posted">Posted</div>
 				</SideBar>
+				<MainPanel>
+					<select onChange={this.selectOnChangeHandler} defaultValue="fashion">
+						<option value="fashion">Fashion</option>
+						<option value="beauty">Beauty</option>
+						<option value="art">Art</option>
+						<option value="books">Books</option>
+						<option value="lifestyle">Lifestyle</option>
+					</select>
+					{
+						this.state.blogs.length !== 0
+						?
+						this.state.blogs.map(blog => {
+							return <Card key={blog._id} blog={blog} />
+						})
+						:
+						null
+					}
+				</MainPanel>
 			</StyledContainer>
 		);
 	}
