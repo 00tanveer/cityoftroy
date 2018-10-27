a8a49ad581755d1cb722275bd9dbdf983a954634
00tanveer <tanveer_fh09@yahoo.com>
2018-10-28 01:03:31 +0600

    added a sidebar

--- src/containers/pages/Admin.js
+++ src/containers/pages/Admin.js
@@ -3,6 +3,10 @@ import withAuth from '../../components/HOC/withAuth';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const StyledContainer = styled.div`
+
+`;
+
 const Title = styled.p`
 	color: white;
 	font-size: 3.5rem;
@@ -15,13 +19,55 @@ const Title = styled.p`
 	cursor: pointer;
 `;
 
-const StyledContainer = styled.div`
-
+const SideBar = styled.div`
+	position: fixed;
+	left: 0;
+	height: 100%;
+	width: 200px;
+	background-color: rgb(8,8,8);
+	.row {
+		color: grey;
+		text-align: center;
+		font-size: 2rem;
+		font-family: 'Raleway';
+		padding: 10px;
+		cursor: pointer;
+		&:hover {
+			color: white;
+		}
+	}
+	#drafts {
+		color: ${props => props.selected === 'drafts' ? 'pink' : 'grey'}
+	}
+	#posted {
+		color: ${props => props.selected === 'posted' ? 'pink' : 'grey'}
+	}
+	.divider {
+		position: relative;
+		right: 50%;
+		transform: translateX(75%);
+		width: 80%;
+		height: 1px;
+		background-color: grey;
+		text-align: center;
+	}
 `;
 
 class Admin extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			selected: 'drafts'
+		}
+		this.rowClickHandler = this.rowClickHandler.bind(this);
+	}
+
+	rowClickHandler() {
+		if (this.state.selected === 'drafts') {
+			this.setState({ selected: 'posted'});
+		} else {
+			this.setState({ selected: 'drafts' });
+		}
 	}
 
 	render() {
@@ -30,6 +76,11 @@ class Admin extends React.Component {
 				<Link style={{ textDecoration: 'none'}} to="/" >
 					<Title>City of Troy</Title>
 				</Link>
+				<SideBar selected={this.state.selected}>
+					<div onClick={this.rowClickHandler} className="row" id="drafts">Drafts</div>
+					<div className="divider"></div>
+					<div onClick={this.rowClickHandler} className="row" id="posted">Posted</div>
+				</SideBar>
 			</StyledContainer>
 		);
 	}
